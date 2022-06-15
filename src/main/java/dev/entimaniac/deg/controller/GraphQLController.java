e89ed591060c9c2a2082ee47a9bc3b6a9839f7bb
Jesee Strode <jesse_strode@condenast.com>
2022-06-15 16:12:56 -0400

    clean up controller

--- src/main/java/dev/entimaniac/deg/controller/GraphQLController.java
+++ src/main/java/dev/entimaniac/deg/controller/GraphQLController.java
@@ -1,13 +1,10 @@
 package dev.entimaniac.deg.controller;
 
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,12 +14,8 @@ import dev.entimaniac.deg.graphql.PersonGraphQL;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
-import graphql.execution.AsyncExecutionStrategy;
-import graphql.execution.AsyncSerialExecutionStrategy;
 import graphql.schema.GraphQLSchema;
 import io.leangen.graphql.GraphQLSchemaGenerator;
-import io.leangen.graphql.metadata.strategy.query.AnnotatedResolverBuilder;
-import io.leangen.graphql.metadata.strategy.value.jackson.JacksonValueMapperFactory;
 
 @RestController
 public class GraphQLController {
@@ -31,42 +24,23 @@ public class GraphQLController {
     public GraphQLController(PersonGraphQL personGraphQL) {
         GraphQLSchema schema =
                 new GraphQLSchemaGenerator()
-                        .withResolverBuilders(new AnnotatedResolverBuilder())
+                        .withBasePackages("dev.entimaniac.deg")
                         .withOperationsFromSingleton(personGraphQL)
-                        .withValueMapperFactory(new JacksonValueMapperFactory())
                         .generate();
 
-        graphQL =
-                GraphQL.newGraphQL(schema)
-                        .queryExecutionStrategy(new AsyncExecutionStrategy())
-                        .mutationExecutionStrategy(new AsyncSerialExecutionStrategy())
-                        .build();
+        graphQL = GraphQL.newGraphQL(schema).build();
     }
 
-    /**
-     * This method is responsible for acting as an endpoint and handling graphql requests.
-     *
-     * @param request the request map
-     * @param raw     the raw http request
-     * @return the Map having the key as string and value as Object and finally converted into the
-     * JSON string
-     */
-    @CrossOrigin
-    @PostMapping(
-            value = "/graphql",
-            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+
+    @PostMapping(value = "/graphql", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Map<String, Object> graphql(
-            @RequestBody Map<String, String> request, HttpServletRequest raw)
-            throws ExecutionException, InterruptedException {
-        CompletableFuture<ExecutionResult> promise =
-                graphQL.executeAsync(
-                        ExecutionInput.newExecutionInput()
-                                .query(request.get("query"))
-                                .operationName(request.get("operationName"))
-                                .context(raw)
-                                .build());
-        return promise.get().toSpecification();
+    public Map<String, Object> graphql(@RequestBody Map<String, String> request, HttpServletRequest raw) {
+        ExecutionResult executionResult = graphQL.execute(ExecutionInput.newExecutionInput()
+                .query(request.get("query"))
+                .operationName(request.get("operationName"))
+                .context(raw)
+                .build());
+        return executionResult.toSpecification();
     }
+
 }
